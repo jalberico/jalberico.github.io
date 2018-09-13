bc62c1cb69ef31a9dd1e1294c4facb7bb79313ad
jalberico <43248510+jalberico@users.noreply.github.com>
2018-09-13 12:25:50 -0500

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,19 @@
+
+
+function addNumbers(p1,p2)
+{
+    return p1+p2;
+}
+
+function multiplyNumbers(p1,p2)
+{
+    return p1*p2;
+}
+
+function mathOps(num1,num2)
+{
+    var sum = addNumbers(num1,num2);
+    var product = multiplyNumbers(num1,num2);
+    return sum + product;
+
+}
